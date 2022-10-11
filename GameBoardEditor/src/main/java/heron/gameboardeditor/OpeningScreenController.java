a188815fb51fe5cb8a9eb5ae282e77b39e4db794
Ezana <ezanaseyoum20@augustana.edu>
2022-10-10 22:30:51 -0500

    Created welcome screen and edited opening screen

--- GameBoardEditor/src/main/java/heron/gameboardeditor/OpeningScreenController.java
+++ GameBoardEditor/src/main/java/heron/gameboardeditor/OpeningScreenController.java
@@ -3,36 +3,55 @@ package heron.gameboardeditor;
 import java.io.IOException;
 
 import javafx.application.Platform;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import java.net.URL;
-import java.util.ResourceBundle;
 import javafx.scene.control.Button;
 import javafx.scene.paint.Color;
 import javafx.scene.text.Font;
 
 public class OpeningScreenController {
-
-    @FXML private Button copyButton;
-    @FXML private Button deleteButton;
-    @FXML private Button editButton;
-    @FXML private Button exitButton;
-    @FXML private Button exportButton;
-    @FXML private Button minimizeButton;
-    @FXML private Font x1;
-    @FXML private Color x2;
-    @FXML private Color x21;
-    @FXML private Font x3;
-    @FXML private Color x4;
-    
-    @FXML
+	
+	@FXML
     private void switchToSecondary() throws IOException {
         App.setRoot("welcomeScreen");
     }
-    
+
+    @FXML // fx:id="copyButton"
+    private Button copyButton; // Value injected by FXMLLoader
+
+    @FXML // fx:id="deleteButton"
+    private Button deleteButton; // Value injected by FXMLLoader
+
+    @FXML // fx:id="editButton"
+    private Button editButton; // Value injected by FXMLLoader
+
+    @FXML // fx:id="exitButton"
+    private Button exitButton; // Value injected by FXMLLoader
+
+    @FXML // fx:id="exportButton"
+    private Button exportButton; // Value injected by FXMLLoader
+
+    @FXML // fx:id="minimizeButton"
+    private Button minimizeButton; // Value injected by FXMLLoader
+
+    @FXML // fx:id="x1"
+    private Font x1; // Value injected by FXMLLoader
+
+    @FXML // fx:id="x2"
+    private Color x2; // Value injected by FXMLLoader
+
+    @FXML // fx:id="x21"
+    private Color x21; // Value injected by FXMLLoader
+
+    @FXML // fx:id="x3"
+    private Font x3; // Value injected by FXMLLoader
+
+    @FXML // fx:id="x4"
+    private Color x4; // Value injected by FXMLLoader
+
     @FXML
-    private void exitTheSceen() {
+    void exitTheSceen(ActionEvent event) {
     	Platform.exit();
     }
 
 }
-
