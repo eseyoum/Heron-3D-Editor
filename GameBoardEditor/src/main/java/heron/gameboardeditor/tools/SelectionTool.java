50fe83312726ed5a190518861ed192249695ea69
StephanieNhiLe <stephaniele20@augustana.edu>
2022-11-30 01:49:49 -0600

    Refactoring Selection Tool to work properly with other tools

--- GameBoardEditor/src/main/java/heron/gameboardeditor/tools/SelectionTool.java
+++ GameBoardEditor/src/main/java/heron/gameboardeditor/tools/SelectionTool.java
@@ -20,7 +20,7 @@ public class SelectionTool extends Tool {
 	private Set<CellUI>selectedCells = new HashSet<CellUI>();
 	
 
-	private boolean pressedInCell;
+	private boolean pressedInSelectedCell;
 	private Point2D origin;
 		
 	public SelectionTool(GridBoardUI gridBoard, UndoRedoHandler handler) {
@@ -30,14 +30,13 @@ public class SelectionTool extends Tool {
 		selectionRectangle.setStroke(Color.BLACK);
 		selectionRectangle.setFill(Color.TRANSPARENT);
 		selectionRectangle.getStrokeDashArray().addAll(5.0, 5.0);
-		pressedInCell = false;
+		pressedInSelectedCell = false;
 		origin = new Point2D(0, 0);
 		gridBoard.getChildren().add(selectionRectangle);
 	}
 	
 	@Override
 	public void mousePressed(MouseEvent e) {
-		
 		initialSelectX = e.getX();
 		initialSelectY = e.getY();
 		
@@ -46,44 +45,33 @@ public class SelectionTool extends Tool {
 		selectionRectangle.setWidth(0);
 		selectionRectangle.setHeight(0);
 		selectionRectangle.setVisible(true);
-		deselectAll();
-		
-		CellUI clickedCell = getCellContaining((int) e.getX(), (int) e.getY());
-		
-		System.out.println("press cell test");
-		pressedInCell = (clickedCell != null);
-		if (!pressedInCell)
+		CellUI cellClicked = getCellContaining((int) e.getX() / CellUI.TILE_SIZE, (int) e.getY() / CellUI.TILE_SIZE);
+		pressedInSelectedCell = (cellClicked.isSelected());
+		if (!pressedInSelectedCell)
 			deselectAll();
-		if (pressedInCell && !clickedCell.isSelected())
+		if (pressedInSelectedCell && !cellClicked.isSelected()) 
 			deselectAll();
-		if (pressedInCell)
-			clickedCell.setSelected(true);
-	
+		if (pressedInSelectedCell)
+			cellClicked.setSelected(true);
 		origin = new Point2D(e.getX(), e.getY());
 	}
 	
 	@Override
 	public void mouseDragged(MouseEvent e) {
-		if (pressedInCell) {
-			initialSelectX = e.getSceneX();
-			initialSelectY = e.getSceneY();
-			int x = (int) ((initialSelectX / CellUI.TILE_SIZE) % (gridBoard.getWidth() / CellUI.TILE_SIZE) * CellUI.TILE_SIZE);
-			int y = (int) ((initialSelectY / CellUI.TILE_SIZE) % (gridBoard.getHeight() / CellUI.TILE_SIZE) * CellUI.TILE_SIZE);
+		if (pressedInSelectedCell) {
+			System.out.println("Drag test - cell is pressed or not");
+			int x = (int) (((e.getX() / CellUI.TILE_SIZE) % (gridBoard.getWidth() / CellUI.TILE_SIZE)) * CellUI.TILE_SIZE);
+			int y = (int) (((e.getY() / CellUI.TILE_SIZE) % (gridBoard.getHeight() / CellUI.TILE_SIZE)) * CellUI.TILE_SIZE);
 			moveSelectedCells(x - origin.getX(), y - origin.getY());
-//			for (CellUI cell: selectedCells) {
-//				cell.setLayoutX(x - origin.getX());
-//				cell.setLayoutY(y - origin.getY());
-//			}
 			origin = new Point2D(x,y);
-		} 
-
+		} else {
 			selectionRectangle.setX(Math.min(e.getX(), initialSelectX));
 			selectionRectangle.setWidth(Math.abs(e.getX() - initialSelectX));
 			selectionRectangle.setY(Math.min(e.getY(), initialSelectY));
 			selectionRectangle.setHeight(Math.abs(e.getY() - initialSelectY));
-		
-	}
 	
+		}
+	}
 	
 	@Override
 	public void mouseReleased(MouseEvent e) {
@@ -106,7 +94,6 @@ public class SelectionTool extends Tool {
     	for (CellUI cell: selectedCells) {
     		cell.deselect();
     		cell.setSelected(false);
-    		
     	}
     	selectedCells.clear();
     }
@@ -128,8 +115,8 @@ public class SelectionTool extends Tool {
 		for (CellUI cell : selectedCells) {
 			if (cell.isSelected()) {
 				cell = new CellUI(gridBoard, cell.getBlock());
-				cell.setX(cell.getX() + dx);
-				cell.setY(cell.getY() + dy);
+				cell.setLayoutX(cell.getX() + dx);
+				cell.setLayoutY(cell.getY() + dy);
 			}
 		}
 	}
