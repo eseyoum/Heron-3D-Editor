c5d4333aaca11b1c60b3a263361162c7d01888fa
Corey Reagan <coreyreagan20@augustana.edu>
2022-12-09 23:06:45 -0600

    added JUnit testing for the CellUI class with the generateColors() method

---
+++ GameBoardEditor/src/test/java/heron/gameboardeditor/CellUITest.java
@@ -0,0 +1,42 @@
+package heron.gameboardeditor;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.jupiter.api.Test;
+
+import javafx.scene.paint.Color;
+
+class CellUITest {
+
+	@Test
+	void testGenerateColors() {
+    	int maxLevel = 6;
+    	double brightness = .2;
+    	double brightnessIncrease = ((double) 1 - brightness) / ((double) maxLevel - (double) 1); //how much each level's color should be increased from the previous
+    	for (int i = 0; i < maxLevel - 1; i++) {
+        	brightness = brightness + brightnessIncrease;
+        }
+    	assertEquals(brightness, 1);
+    	
+    	maxLevel = 5;
+    	brightness = .2;
+    	brightnessIncrease = ((double) 1 - brightness) / ((double) maxLevel - (double) 1); //how much each level's color should be increased from the previous
+    	for (int i = 0; i < maxLevel - 1; i++) {
+        	brightness = brightness + brightnessIncrease;
+        }
+    	assertEquals(brightness, 1);
+    	
+    	maxLevel = 5;
+    	brightness = .3;
+    	brightnessIncrease = ((double) 1 - brightness) / ((double) maxLevel - (double) 1); //how much each level's color should be increased from the previous
+    	for (int i = 0; i < maxLevel - 1; i++) {
+        	brightness = brightness + brightnessIncrease;
+        }
+    	assertEquals(brightness, 1);
+	}
+	
+
+}
